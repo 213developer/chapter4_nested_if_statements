3716bab387c3c08c5628f2b28f0997631ff17558
“javascriptseo” <seofreelance2021@outlook.com>
2023-09-22 22:53:30 -0700

    code updates

--- nested if statements.py
+++ nested if statements.py
@@ -16,8 +16,12 @@ dollarString = input("Enter transactions dollar value: ")
 numShifts = float(shiftString)
 numTransactions = float(transactString)
 dollarValue = float(dollarString)
+
 # Write your code here
+# A productivity score is dividing an employees transactions dollar value by the number of transactions and then dividing the result by the number of shifts worked
+bonus = (dollarValue/numTransactions)/
 
 # Output.
+# print(productivityScore)
 print("Employee Name: " + employeeName)
 print("Employee Bonus: $" + str(bonus))
